e14e87540283a111e4acf294affa1b6f1cca0c60
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2024-05-03 11:20:41 +0800

    feat: add signal handler

--- main.cc
+++ main.cc
@@ -4,9 +4,22 @@
 #include <fmt/ostream.h>
 #include <fmt/ranges.h>
 
+#include <csignal>
+#include <sys/signal.h>
+
 #include <iostream>
 #include <memory>
 #include <vector>
+
+volatile sig_atomic_t g_stop = 0;
+
+void sigint_handler(int) {
+  fmt::println("SIGINT received.");
+  g_stop = 1;
+}
+
+const uint32_t FPS = 5;
+const uint32_t nProducers = 5;
 
 class StoppableThread {
   std::unique_ptr<std::thread> thread_;
@@ -14,8 +27,9 @@ class StoppableThread {
 
 public:
   explicit StoppableThread() : thread_(nullptr), stop_(false) {}
+  virtual ~StoppableThread() = default;
   void start() {
-    std::cout << "start" << std::endl;
+    fmt::println("{} started.", name());
     thread_ = std::make_unique<std::thread>(&StoppableThread::main, this);
   }
   void main() {
@@ -29,7 +43,7 @@ public:
   void wait() {
     if (thread_ && thread_->joinable()) {
       thread_->join();
-      std::cout << "finish" << std::endl;
+      fmt::println("{} finished.", name());
     }
   }
 };
@@ -55,8 +69,8 @@ public:
       : q_(q), q_out_(q_out) {}
   std::string name() final { return "Consumer"; }
   void run() final {
-    std::vector<Frame> v(20);
-    std::size_t count = q_.try_dequeue_bulk(v.begin(), 20);
+    std::vector<Frame> v(nProducers * 2);
+    std::size_t count = q_.try_dequeue_bulk(v.begin(), v.size());
     if (count > 0) {
       fmt::println("{} dequeued {}.", name(),
                    std::vector<Frame>(v.begin(), v.begin() + count));
@@ -65,7 +79,7 @@ public:
         q_out_[v[i].producer_id].try_enqueue(v[i]);
       }
     } else {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FPS));
     }
   }
 };
@@ -88,7 +102,7 @@ public:
       fmt::println("{} failed enqueue.", name());
     }
     frame_ += 1;
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FPS));
   }
 };
 
@@ -100,41 +114,60 @@ public:
   PostConsumer(moodycamel::ConcurrentQueue<Frame> &q_map, int id)
       : q_(q_map), id_(id) {}
   void run() final {
-    Frame frame;
-    bool success = q_.try_dequeue(frame);
-    if (success) {
-      fmt::println("{} dequeued {}.", name(), frame);
+    std::vector<Frame> frames(FPS);
+    int count = q_.try_dequeue_bulk(frames.begin(), frames.size());
+    if (count > 0) {
+      fmt::println("{} dequeued {}.", name(),
+                   std::vector<Frame>(frames.begin(), frames.begin() + count));
     } else {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FPS));
     }
   }
   std::string name() final { return fmt::format("PostConsumer: {}", id_); }
 };
 
 int main(int argc, char *argv[]) {
-  moodycamel::ConcurrentQueue<Frame> q(32 * 10);
+  // register signal handler
+  {
+    struct sigaction sig_int_act;
+    sig_int_act.sa_handler = sigint_handler;
+    sigemptyset(&sig_int_act.sa_mask);
+    sig_int_act.sa_flags = 0;
+    sigaction(SIGINT, &sig_int_act, nullptr);
+  }
+
+  // collect all threads
+  std::vector<std::unique_ptr<StoppableThread>> threads;
+
+  moodycamel::ConcurrentQueue<Frame> q_in(32 * 10);
   std::unordered_map<int, moodycamel::ConcurrentQueue<Frame>> q_out;
-  Consumer consumer(q, q_out);
-  Producer p1(q, 1);
-  Producer p2(q, 2);
-  q_out.insert({1, moodycamel::ConcurrentQueue<Frame>()});
-  q_out.insert({2, moodycamel::ConcurrentQueue<Frame>()});
-  PostConsumer pc1(q_out[1], 1);
-  PostConsumer pc2(q_out[2], 2);
-  consumer.start();
 
-  std::vector<StoppableThread *> producers = {&p1, &p2, &pc1, &pc2};
-  for (auto &p : producers)
-    p->start();
+  std::unique_ptr<Consumer> consumer = std::make_unique<Consumer>(q_in, q_out);
+  threads.push_back(std::move(consumer));
 
-  std::cin.get();
+  for (int i = 0; i < nProducers; i++) {
+    std::unique_ptr<Producer> p = std::make_unique<Producer>(q_in, i);
+    q_out.insert({i, moodycamel::ConcurrentQueue<Frame>()});
+    std::unique_ptr<PostConsumer> pc =
+        std::make_unique<PostConsumer>(q_out[i], i);
+    threads.push_back(std::move(p));
+    threads.push_back(std::move(pc));
+  }
 
-  for (auto &p : producers)
+  // start all threads
+  for (auto &t : threads)
+    t->start();
+
+  // signal handler
+  while (!g_stop) { // press ctrl-c to stop
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  // graceful stop all threads
+  for (auto &p : threads)
     p->stop();
-  for (auto &p : producers)
+  for (auto &p : threads)
     p->wait();
 
-  consumer.stop();
-  consumer.wait();
   return 0;
 }
